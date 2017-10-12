630258f5bea6735629b79bdadc7ca1e94e206841
AJ Pahl <aj@AJ-MBP-Elastic.local>
2017-10-11 21:15:15 -0400

    added single-multi service scripts

---
+++ app-multi-services.js
@@ -0,0 +1,74 @@
+var apm = require('elastic-apm').start({
+    appName: 'apm-nodejs-test',
+    secretToken: '',
+    serverUrl: 'http://localhost:8200'
+})
+
+var app = require('express')()
+
+var responseCodeArray = [ 100, 500, 400, 300, 200 ];
+
+function getRandomResponseCode() {
+    return responseCodeArray[Math.floor(Math.random() * 4) + 1];
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+function getRandomSleepTime(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// any errors caught by Express can be logged by the agent as well
+app.use(apm.middleware.express())
+
+app.get('/reviewCart', function (req, res) {
+    apm.startTransaction("ReviewCart", "HTTP");
+
+    var randomSleepTime = getRandomSleepTime(1,5000);
+    sleep(randomSleepTime);
+ 
+    var responseCode = getRandomResponseCode();
+    console.log("ReviewCart: ResponseCode: " + responseCode + " Synth Delay Time: " + randomSleepTime );
+    res.status(responseCode).send('You have no items.  Go pick stuff!!!\n')
+    apm.endTransaction()
+
+})
+
+app.get('/submitOrder', function (req, res) {
+    apm.startTransaction("SubmitOrder", "HTTP");
+
+    var randomSleepTime = getRandomSleepTime(1,5000);
+    sleep(randomSleepTime);
+ 
+    var responseCode = getRandomResponseCode();
+    console.log("SubmitOrder: ResponseCode: " + responseCode + " Synth Delay Time: " + randomSleepTime );
+    res.status(responseCode).send('Order is on the way!\n')
+    apm.endTransaction()
+
+})
+
+app.get('/login', function (req, res) {
+    apm.startTransaction("Login", "HTTP");
+
+    var randomSleepTime = getRandomSleepTime(1,2000);
+    sleep(randomSleepTime);
+ 
+    var responseCode = getRandomResponseCode();
+    console.log("SubmitOrder: ResponseCode: " + responseCode + " Synth Delay Time: " + randomSleepTime );
+    res.status(responseCode).send('Welcome to Awesome!!!\n')
+    apm.endTransaction()
+
+})
+
+app.listen(3000)
+
+console.log("app-multi-service.js started and listening...")
---
+++ app-single-service.js
@@ -0,0 +1,57 @@
+var apm = require('elastic-apm').start({
+    appName: 'apm-nodejs-test',
+    secretToken: '',
+    serverUrl: 'http://localhost:8200'
+})
+
+var app = require('express')()
+
+function simulateException() {
+   try {
+       adddlert("Welcome");
+   }
+    catch (err) {
+        apm.captureError('Exception Occured  happened!')
+    }
+}
+
+var responseCodeArray = [ 100, 500, 400, 300, 200 ];
+
+function getRandomResponseCode() {
+    return responseCodeArray[Math.floor(Math.random() * 4) + 1];
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+function getRandomSleepTime(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// any errors caught by Express can be logged by the agent as well
+app.use(apm.middleware.express())
+
+app.get('/', function (req, res) {
+    apm.startTransaction("KeepAlive", "HTTP");
+
+    var randomSleepTime = getRandomSleepTime(1,5000);
+    sleep(randomSleepTime);
+ 
+    var responseCode = getRandomResponseCode();
+    console.log("KeepAlive: ResponseCode: " + responseCode + " Synth Delay Time: " + randomSleepTime );
+    res.status(responseCode).send('Service Available\n')
+    apm.endTransaction()
+
+})
+
+app.listen(3000)
+
+console.log("app-single-service.js started and listening...")
